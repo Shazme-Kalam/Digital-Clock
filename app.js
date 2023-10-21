9c64c84c79102903b787167fea803359a3492628
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2023-10-21 06:40:20 -0700

    Display real Date, Day and Time

---
+++ app.js
@@ -0,0 +1,35 @@
+function updateClock(){
+var date = new Date();
+var day = date.getDay();
+    mon =date.getMonth();
+    dNum = date.getDate();
+    yr = date.getFullYear();
+    hour = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    per = "AM"
+ 
+if(hour == 0){
+    hour = hour - 12;
+    per = "PM"
+}
+
+Number.prototype.pad = function(digits){
+    for(var n = this.toString(); n.length < digits; n= 0 + n);
+    return n;
+}
+
+    var months = ["January", "February", "March", "April", "May","June", "July", "August", "September", "October","November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+    var values = [week[day], months[mon], dNum.pad(2), yr, hour.pad(2), min.pad(2), sec.pad(2), per];
+    for(var i = 0; i < ids.length; i++){
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+    }
+
+}
+
+function initClock(){
+    updateClock();
+    window.setInterval("updateClock()",1)
+}
